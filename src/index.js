2ff2b088a11a058558464373900e78868446fcc4
KateKurakina <58449858+KateKurakina@users.noreply.github.com>
2021-03-14 14:47:33 +0300

    init: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let rev = 0;
+    let dig = 0;
+    while (n !== 0)
+    {
+        dig = n % 10;
+        rev = rev * 10 + dig;
+        n = Math.floor(n / 10);
+    }
+    return rev;
 }
